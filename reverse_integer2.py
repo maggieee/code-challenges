4514eb997ab000b6ca46d2b7a0a07bb0dc378d42
maggieee <margaret.ronan@gmail.com>
2020-09-01 17:06:40 +0000

    begin reverse integer code challenge

---
+++ reverse_integer2.py
@@ -0,0 +1,37 @@
+# Given a 32-bit signed integer, reverse digits of an integer.
+
+# Example 1:
+
+# Input: 123
+# Output: 321
+
+# Example 2:
+
+# Input: -123
+# Output: -321
+
+# Example 3:
+
+# Input: 120
+# Output: 21
+# Note:
+# Assume we are dealing with an environment which could only store integers within the 32-bit 
+# signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function 
+# returns 0 when the reversed integer overflows.
+
+def reverse_integer(num):
+
+    is_neg = False
+
+    if num[0] == "-":
+        is_neg = True
+
+    
+
+
+    if is_neg:
+        # add "-" at beginning of number
+
+        return num
+
+print(reverse_integer(123) == "321")
